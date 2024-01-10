cbd8af65026490c04626089c9d06357a80b1c23c
Mike5719 <149823846+Mike5719@users.noreply.github.com>
2024-01-10 00:34:54 -0500

    updates

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -20,7 +20,7 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id` - done
   // TODO: be sure to include its associated Category and Tag data
   try {
-    const productData = await Traveller.findByPk(req.params.id);
+    const productData = await Product.findByPk(req.params.id);
 
     if (!productData) {
       res.status(404).json({ message: 'No product found with this id!' });
